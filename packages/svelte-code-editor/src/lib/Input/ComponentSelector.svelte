405cb1b449ed5e33d7cb7662cf0c8793bc830c87
umaranis <mail@umaranis.com>
2025-03-26 17:09:06 +1100

    replace background image URL with inline SVG data in ComponentSelector

--- packages/svelte-code-editor/src/lib/Input/ComponentSelector.svelte
+++ packages/svelte-code-editor/src/lib/Input/ComponentSelector.svelte
@@ -234,7 +234,7 @@
 			position: absolute;
 			left: 0em;
 			top: 0;
-			background: url(./file.svg) 50% 50% no-repeat;
+			background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='%23676778' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z'%3E%3C/path%3E%3Cpolyline points='14 2 14 8 20 8'%3E%3C/polyline%3E%3C/svg%3E") 50% 50% no-repeat;
 			background-size: 1em;
 		}
 
