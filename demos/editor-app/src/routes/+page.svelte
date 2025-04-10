947c9fcf4e793027aa238cb1bd7009315d1ab546
umaranis <mail@umaranis.com>
2025-04-10 21:26:56 +1000

    update welcome message in the demo app

--- demos/editor-app/src/routes/+page.svelte
+++ demos/editor-app/src/routes/+page.svelte
@@ -19,8 +19,7 @@
  });
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to svelte-code-editor</h1>
 
 <TabbedEditor {workspace} />
 
